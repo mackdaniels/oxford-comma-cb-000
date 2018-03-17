1c1161a6fce4a1ce29ddcd8597635323467eaeee
Nicholas Daniel <mackdaniels@protonmail.com>
2018-03-17 23:56:08 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,8 +6,8 @@ def oxford_comma(array)
   elsif array.length == 3
     return "#{array[0]}, #{array[1]}, and #{array[2]}"
   elsif array.length > 3
-    array.join(", ")
-    puts array
+    array = array.join(", ")
+    return array
   else
     return false
   end

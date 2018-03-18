77921e89af87166862b58b135451df20fc5339c9
Nicholas Daniel <mackdaniels@protonmail.com>
2018-03-18 00:00:32 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+  if array.length == 1
+    return array.join("")
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  elsif array.length == 3
+    return "#{array[0]}, #{array[1]}, and #{array[2]}"
+  elsif array.length > 3
+    array[-1] = "and #{array[-1]}"
+    array = array.join(", ")
+    return array
 
+  else
+    return false
+  end
 end
